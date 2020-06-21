f5ad7ae85052ec4047d07e3f5e43f312dd791520
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-06-21 18:46:52 -0400

    Spring boot app initial skeleton

---
+++ src/main/java/com/my/poc/pdfgenerationitext/PdfGenerationItextApplication.java
@@ -0,0 +1,13 @@
+package com.my.poc.pdfgenerationitext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfGenerationItextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfGenerationItextApplication.class, args);
+	}
+
+}
